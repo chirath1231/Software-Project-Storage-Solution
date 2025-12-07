97bad0dcdf1a2033c960490f74cd36c08485aa01
chirath1231 <chirathhettiarachchi761@gmail.com>
2025-12-07 22:52:28 +0530

    zzz

--- backend/subscriptions/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.subscription_list, name="subscription-list"),
-    path("create-payment/", views.create_payhere_payment, name="create-payhere-payment"),
-    path("payhere/notify/", views.payhere_notify, name="payhere-notify"),
-    path("payment-status/<str:order_id>/", views.check_payment_status, name="payment-status"),
-]
