2773718bcc43bc136e25a8c9e5e904f3105b33d0
Osal Geesara <geesarahko.23@uom.lk>
2025-12-13 01:59:09 +0530

    Add Sidebar component to Dashboard page for improved navigation

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,18 +1,18 @@
 import { Link } from "react-router-dom";
 import Navbar from "../components/NavBar/NavBar";
 import Footer from "../components/Footer/Footer";
+import Sidebar from "../components/Sidebar/Sidebar";
 
 function Dashboard() {
   return (
+  
     <div>
       <Navbar />
-<div>
-
-    <h1>Dashboard</h1>
-
-</div>
+      <Sidebar />
       <Footer />
     </div>
+
+   
   );
 }
 
