4b4ec3d211ef2ceb5836cbda6209720175fc04c4
chirath1231 <chirathhettiarachchi761@gmail.com>
2026-03-22 18:20:06 +0530

    change gitignore file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,46 +6,29 @@ import SubscriptionPage from "./pages/SubscriptionPage.jsx";
 import PaymentSuccess from "./pages/PaymentSuccess.jsx";
 import { AuthProvider } from "./auth/AuthContext.jsx";
 import ProtectedRoute from "./routes/ProtectedRoute";
-<<<<<<< HEAD
-import Dashboard from "./pages/Dashboard.jsx";
-import ClientChatSystem from "./pages/ClientChatSystem.jsx";
-=======
 import DashboardLayout from "./layouts/DashboardLayout";
 import DashboardHome from "./pages/DashboardHome";
 import MyFiles from "./pages/MyFiles";
-<<<<<<< HEAD
-import Notifications from './pages/Notifications';
-import MyProfile from './pages/MyProfile';
->>>>>>> 09dd24186fec7dc528f4ce694b942caf1035fecd
-=======
->>>>>>> chirath
 
 function App() {
   return (
-
+    
     <BrowserRouter>
       <AuthProvider>
         <Routes>
-          {/* PUBLIC PAGES */}
+          {/* PUBLIC */}
           <Route path="/" element={<Landing />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
 
-          {/* PROTECTED PAGES */}
+          {/* PROTECTED */}
           <Route element={<ProtectedRoute />}>
-<<<<<<< HEAD
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/subscription" element={<SubscriptionPage />} />
-            <Route path="/payment-success" element={<PaymentSuccess />} />
-            <Route path="/clientchatsystem" element={<ClientChatSystem />} />
-=======
             <Route path="/dashboard" element={<DashboardLayout />}>
               <Route path="files" element={<MyFiles />} />
               <Route index element={<DashboardHome />} />
               <Route path="subscription" element={<SubscriptionPage />} />
               <Route path="payment-success" element={<PaymentSuccess />} />
             </Route>
->>>>>>> 09dd24186fec7dc528f4ce694b942caf1035fecd
           </Route>
         </Routes>
       </AuthProvider>
