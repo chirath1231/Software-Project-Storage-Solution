54e959508a9bd4d0d15e7b749f3f8685751a9eaa
Osal Geesara <geesarahko.23@uom.lk>
2026-05-11 23:15:28 +0530

    Add DeleteMessageView and update API routes; enhance ChatConsumer to handle message deletion

--- backend/chat/api_urls.py
+++ backend/chat/api_urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .api_views import ConversationListView, MessageListView, SendMessageView, StartConversationView
+from .api_views import ConversationListView, MessageListView, SendMessageView, StartConversationView,DeleteMessageView
 
 urlpatterns = [
     path("conversations/", ConversationListView.as_view()),
     path("conversations/start/", StartConversationView.as_view()),  # ✅ must be here
     path("messages/<int:conversation_id>/", MessageListView.as_view()),
     path("messages/send/", SendMessageView.as_view()),
+    path('messages/<int:message_id>/delete/', DeleteMessageView.as_view(), name='delete-message'),
 ]
