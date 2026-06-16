a2aae2bf6853b91329a8b3a6f3ec18f25d1dccf0
chirath1231 <chirathhettiarachchi761@gmail.com>
2026-06-16 20:25:32 +0530

    delete all file

--- backend/storage/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import upload_file, list_files, delete_file
-
-urlpatterns = [
-    path("files/", list_files),
-    path("files/upload/", upload_file),
-    path("files/<int:id>/", delete_file),
-]
