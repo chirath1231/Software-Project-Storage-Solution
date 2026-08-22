823c9ebea18127aaed56cde3c063b5527c6f3fb3
Osal Geesara <geesarahko.23@uom.lk>
2026-08-22 21:39:58 +0530

    fix(admin-layout): restructure AdminLayout to render top Navbar full-width above Sidebar

--- frontend/src/layouts/AdminLayout.jsx
+++ frontend/src/layouts/AdminLayout.jsx
@@ -6,21 +6,21 @@ import Footer from "../components/Footer/Footer";
 
 export default function AdminLayout() {
   return (
-    <div className="flex min-h-screen bg-gray-100">
-      {/* Sidebar with isAdmin=true to show admin tabs */}
-      <Sidebar isAdmin={true} />
+    <div className="flex flex-col min-h-screen bg-gray-100">
+      {/* Top Navbar spanning full width across top of page */}
+      <Navbar isDashboard={true} />
 
-      <div className="flex-1 flex flex-col">
-        {/* Navbar with isDashboard=true to show search/profile */}
-        <Navbar isDashboard={true} />
+      {/* Middle row: Sidebar on left + Page content on right */}
+      <div className="flex flex-1">
+        <Sidebar isAdmin={true} />
 
-        {/* Dynamic Page Content */}
-        <main className="flex-1 p-6">
+        <main className="flex-1 p-6 overflow-auto">
           <Outlet />
         </main>
-
-        <Footer />
       </div>
+
+      {/* Bottom Footer spanning full width across bottom of page */}
+      <Footer />
     </div>
   );
 }
